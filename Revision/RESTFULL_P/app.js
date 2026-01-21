c0fab6f7f761921622db8746b95a9aa1ffced8b3
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-01-21 21:22:14 +0530

    Add user management features: create new user form and handle user submissions

--- Revision/RESTFULL_P/app.js
+++ Revision/RESTFULL_P/app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 const users = require('./data/user.js');
 app.use(express.static("public"));
@@ -14,6 +15,20 @@ app.get('/',(req,res)=>{
 
 app.get('/users',(req,res)=>{
     res.render('users',{users});
+})
+
+app.get('/users/new',(req,res)=>{
+    res.render('new');
+})
+
+app.post('/users',(req,res)=>{
+    console.log(req.body);
+    const{name,age,course,password}= req.body;
+    users.push({
+        id:users[users.length-1].id+1,
+        name,age,course,password
+    })
+    res.redirect('/users')
 })
 
 app.listen(4000,()=>{
--- Revision/RESTFULL_P/data/user.js
+++ Revision/RESTFULL_P/data/user.js
@@ -1,31 +1,36 @@
 const user = [
     {
+    
         id : 12,
-        Name : 'Manish Bhardwaj',
-        Age : 19,
-        Course : 'Btech cs AIML'
+        name : 'Manish Bhardwaj',
+        age : 19,
+        password : 'man',
+        course : 'Btech cs AIML'
 
     },
 
     {
         id : 13,
-        Name : 'Anubhav',
-        Age : 19,
-        Course : 'Btech cs '
+        name : 'Anubhav',
+        age : 19,
+        password : 'anu',
+        course: 'Btech cs '
     },
 
     {
         id : 14,
-        Name : 'Aashish',
-        Age : 19,
-        Course : 'Btech cs'
+        name : 'Aashish',
+        age : 19,
+        password : 'ashi',
+        course : 'Btech cs'
     },
 
     {
         id : 15,
-        Name : 'Abhishek',
-        Age : 19,
-        Course : 'Btech Mechanical'
+        name : 'Abhishek',
+        age : 19,
+        password : 'abhi',
+        course : 'Btech Mechanical'
     }
 ];
 
