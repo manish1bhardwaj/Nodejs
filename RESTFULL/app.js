bdf17f4aa36d15d238aa6223bef102695a4b11e5
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-01-21 11:53:16 +0530

    Implement user creation functionality and add new user form

--- RESTFULL/app.js
+++ RESTFULL/app.js
@@ -1,21 +1,40 @@
-const express =require('express');
+const express = require('express');
 const app = express();
 const path = require('path');
-app.set('view engine','ejs');
-app.set("views",path.join(__dirname,"views"))
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"))
+app.use(express.urlencoded({ extended: true }));
 
 const users = require('./data/users');
 app.use(express.static("public"));
 
-app.get ('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get ('/users',(req,res)=>{
-    res.render('users',{users})
+app.get('/users', (req, res) => {
+    res.render('users', { users })
+})
+
+app.get('/users/new', (req, res) => {
+    res.render('new');
+})
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    const { name, age, city, password } = req.body;
+    const newuser = {
+        id: users.length - 1,
+        name,
+        age,
+        city, password
+    }
+    users.push(newuser);
+    res.redirect('/users');
 })
 
 
-app.listen(4000,()=>{
+
+app.listen(4000, () => {
     console.log('server run successfully at port 4000');
 })
