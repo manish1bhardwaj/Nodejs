8c1488c70ce6fe85596b0c18de6f0eea40cfdcfa
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-01-21 20:38:43 +0530

    upadate

--- RESTFULL/app.js
+++ RESTFULL/app.js
@@ -20,20 +20,15 @@ app.get('/users/new', (req, res) => {
     res.render('new');
 })
 
-app.post('/users', (req, res) => {
+app.post('/users',(req,res)=>{
     console.log(req.body);
-    const { name, age, city, password } = req.body;
-    const newuser = {
-        id: users.length - 1,
-        name,
-        age,
-        city, password
-    }
-    users.push(newuser);
-    res.redirect('/users');
+    const {name,age,city,password} = req.body;
+    users.push({
+        id:users[users.length-1].id+1,
+        name,age,city,password
+    })
+    res.redirect('/users')
 })
-
-
 
 app.listen(4000, () => {
     console.log('server run successfully at port 4000');
