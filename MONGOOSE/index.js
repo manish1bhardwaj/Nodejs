24cc598b2bb7d786af78dca3fbfbc3ac00f34557
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-03-17 11:50:53 +0530

    create Schema

--- MONGOOSE/index.js
+++ MONGOOSE/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/');
+mongoose.connect('mongodb://localhost:27017/Backend')
+    .then(()=>{
+        console.log("Db Connect");
+    })
+    .catch(()=>{
+        console.log("Do not Connect");
+    })
 
+const userSchema = new mongoose.Schema({
+    name:String,
+    age:Number,         //{type:Number,min:18,max:40,require:true}
+    City:String
+})
 
 
 const port = 4000;
