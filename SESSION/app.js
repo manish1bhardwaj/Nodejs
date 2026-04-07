3fb0de443fa56a1c7420644da1e41db90f589f58
Manish Bhardwaj <talktomanishbhardwaj0010@gmail.com>
2026-04-07 11:43:40 +0530

    Session running

--- SESSION/app.js
+++ SESSION/app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+
+app.use(session({
+    secret: "abcd123",
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.get('/', (req, res) => {
+    console.log(req.session);
+    res.send("Home Page");
+});
+
+app.get("/login",(req,res)=>{
+    req.session.user = "abcd";
+    res.send("you are user in");
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
